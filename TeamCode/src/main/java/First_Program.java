9fdf0ce8b994c63cc5b368d462524894d18134ee
raxel5439 <142732509+raxel5439@users.noreply.github.com>
2026-09-15 13:17:52 -0500

    Create First_Program.java
    First java class example Mr Clark

---
+++ TeamCode/src/main/java/First_Program.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+
+@Disabled
+@TeleOp
+public class HelloWorld extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData( "Hello", "World"); //My first Comment
+    }
+    @Override
+    public void loop() {
+    }
+}
